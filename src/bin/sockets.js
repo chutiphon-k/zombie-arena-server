0f605182c7093b0f37cc5a2ffbfc942c540ca85a
Chutiphon.k <chutiphon.k@ku.th>
2017-05-25 22:44:51 +0700

    :heavy_plus_sign: Get user connect to server and Send to client

--- src/bin/sockets.js
+++ src/bin/sockets.js
@@ -2,6 +2,8 @@ import SocketIO from 'socket.io'
 
 let io
 let clients = []
+let enemies = []
+let playerSpawnPoints = []
 
 let init = (server) => {
 	io = new SocketIO(server)
@@ -10,15 +12,49 @@ let init = (server) => {
 		let currentPlayer = {}
 		currentPlayer.name = 'unknown'
 
-		socket.on('player_connect', (data) => {
+		socket.on('player_connect', () => {
 			console.log(`${currentPlayer.name} on: player connect`)
 			clients.map(client => {
 				socket.emit('other_player_connected', client)
-				console.log(`${currentPlayer.name} emit: other player connected: ${JSON.stringify(client)}\n`)
+				console.log(`${currentPlayer.name} emit: other player connected: ${JSON.stringify(client)}`)
 			})
 		})
 
-		socket.on('play', (data) => {})
+		socket.on('play', (data) => {
+			console.log(`${currentPlayer.name} recv: play:: ${JSON.stringify(data)}`)
+			if (clients.length === 0) {
+				// enemies = []
+				// data.enemySpawnPoints.map((enemySpawnPoint) => {
+				// 	enemies.push({
+				// 		name: Math.random().toString(36).substring(7),
+				// 		position: enemySpawnPoint.position,
+				// 		rotation: enemySpawnPoint.rotation,
+				// 		health: 100
+				// 	})
+				// })
+				playerSpawnPoints = []
+				data.playerSpawnPoints.map((_playerSpawnPoint) => {
+					let { position, rotation } = _playerSpawnPoint
+					playerSpawnPoints.push({ position, rotation })
+				})
+			}
+			// let enemiesResponse = { enemies }
+			// console.log(`${currentPlayer.name} emit: enemies: ${JSON.stringify(enemiesResponse)}`)
+			// socket.emit('enemies', enemiesResponse)
+			let randomSpawnPoint = playerSpawnPoints[Math.floor(Math.random() * playerSpawnPoints.length)]
+			let { position, rotation } = randomSpawnPoint
+			currentPlayer = {
+				name: data.name,
+				position,
+				rotation,
+				health: 100
+			}
+			clients.push(currentPlayer)
+			console.log(`${currentPlayer.name} emit: play: ${JSON.stringify(currentPlayer)}`)
+			socket.emit('play', currentPlayer)
+			socket.broadcast.emit('other_player_connected', currentPlayer)
+		})
+
 		socket.on('other_player_connected', (data) => {})
 		socket.on('other_player_disconnected', (data) => {})
 		socket.on('player_move', (data) => {})
