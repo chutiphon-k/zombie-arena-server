2668d2f372fa1451fc450517edd622f67245799b
Chutiphon.k <chutiphon.k@ku.th>
2017-05-25 16:47:46 +0700

    :heavy_plus_sign: Initial socket.on

--- src/bin/sockets.js
+++ src/bin/sockets.js
@@ -1,11 +1,36 @@
 import SocketIO from 'socket.io'
 
 let io
+let clients = []
 
 let init = (server) => {
 	io = new SocketIO(server)
-	io.on('connect', (socket) => {
-		socket.emit('timer', 'hello')
+	io.on('connection', (socket) => {
+		console.log('connected')
+		let currentPlayer = {}
+		currentPlayer.name = 'unknown'
+
+		socket.on('player_connect', (data) => {
+			console.log(`${currentPlayer.name} on: player connect`)
+			clients.map(client => {
+				socket.emit('other_player_connected', client)
+				console.log(`${currentPlayer.name} emit: other player connected: ${JSON.stringify(client)}\n`)
+			})
+		})
+
+		socket.on('play', (data) => {})
+		socket.on('other_player_connected', (data) => {})
+		socket.on('other_player_disconnected', (data) => {})
+		socket.on('player_move', (data) => {})
+		socket.on('player_shoot', (data) => {})
+		socket.on('player_bomb', (data) => {})
+		socket.on('player_jump', (data) => {})
+		socket.on('player_health', (data) => {})
+		socket.on('player_pick_item', (data) => {})
+		socket.on('wave', (data) => {})
+		socket.on('disconnect', (data) => {})
+		// socket.on('player_pick_item', (data) => {})
+		// socket.on('item', (data) => {})
 	})
 }
 
