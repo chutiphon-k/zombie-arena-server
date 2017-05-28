ca2b5fddfcdf7f2c9cddb79147ecd3d16e9096d3
Chutiphon.k <chutiphon.k@ku.th>
2017-05-28 19:06:39 +0700

    :ok_hand: Server finish

--- src/bin/sockets.js
+++ src/bin/sockets.js
@@ -5,24 +5,79 @@ let io
 let clients = []
 let enemies = []
 let playerSpawnPoints = []
+let enemySpawnPoints = []
+let teamName = 'unknow'
+let isGenEnemy = false
+let score = 0
+let currentWave = 1
+let spawnEnemy
+let wave = {
+	1: {
+		n: 10,
+		delay: 4000
+	},
+	2: {
+		n: 15,
+		delay: 3500
+	},
+	3: {
+		n: 20,
+		delay: 3500
+	},
+	4: {
+		n: 30,
+		delay: 3000
+	},
+	5: {
+		n: 40,
+		delay: 3000
+	},
+	6: {
+		n: 45,
+		delay: 3000
+	},
+	7: {
+		n: 50,
+		delay: 2500
+	},
+	8: {
+		n: 50,
+		delay: 2400
+	},
+	9: {
+		n: 50,
+		delay: 2300
+	},
+	10: {
+		n: 60,
+		delay: 2000
+	}
+}
 
 let history = firebase.database().ref(`history`)
+
+let resetData = () => {
+	clients = []
+	enemies = []
+	playerSpawnPoints = []
+	enemySpawnPoints = []
+	teamName = 'unknow'
+	isGenEnemy = false
+	score = 0
+	currentWave = 1
+	clearInterval(spawnEnemy)
+}
 
 let init = (server) => {
 	io = new SocketIO(server)
 	io.on('connection', (socket) => {
 		console.log('connected')
-		let currentPlayer = {}
-		currentPlayer.name = 'unknown'
-
-		history.on('value', (snapshots) => {
-			const result = []
-			snapshots.forEach((snapshot) => {
-				result.push(snapshot.val())
-			})
-			let ranking = result.sort((a, b) => b.score - a.score).slice(0, 10)
-			socket.emit('ranking', ranking)
-		})
+		let currentPlayer = {
+			name: 'unknown',
+			position: [],
+			health: undefined,
+			character: undefined
+		}
 
 		socket.on('player_connect', () => {
 			console.log(`${currentPlayer.name} on: player connect`)
@@ -33,20 +88,18 @@ let init = (server) => {
 		})
 
 		socket.on('play', (data) => {
-			console.log(data)
-			console.log(`${currentPlayer.name} recv: play:: ${JSON.stringify(data)}`)
 			if (clients.length === 0) {
-				enemies = []
-				for (let i = 0; i < 20; i++) {
-					let randomSpawnPoint = data.enemySpawnPoints[Math.floor(Math.random() * data.enemySpawnPoints.length)]
-					let { position, rotation } = randomSpawnPoint
-					enemies.push({
-						name: Math.random().toString(36).substring(7),
-						position,
-						rotation,
-						health: 100
-					})
-				}
+				teamName = data.teamName
+				genEnemy()
+				spawnEnemy = setInterval(() => {
+					if (enemies.filter(enemy => enemy.isDeath !== true).length === 0 && enemies.length !== 0) {
+						if (!isGenEnemy) {
+							isGenEnemy = true
+							genEnemy()
+						}
+					}
+				}, 500)
+				enemySpawnPoints = data.enemySpawnPoints
 				playerSpawnPoints = []
 				data.playerSpawnPoints.map((_playerSpawnPoint) => {
 					let { position, rotation } = _playerSpawnPoint
@@ -62,7 +115,8 @@ let init = (server) => {
 				name: data.name,
 				position,
 				rotation,
-				health: 100
+				health: 100,
+				isDead: false
 			}
 			clients.push(currentPlayer)
 			console.log(`${currentPlayer.name} emit: play: ${JSON.stringify(currentPlayer)}`)
@@ -71,34 +125,88 @@ let init = (server) => {
 		})
 
 		socket.on('player_move', (data) => {
-			console.log(`recv: move: ${JSON.stringify(data)}`)
+			// console.log(`recv: move: ${JSON.stringify(data)}`)
 			currentPlayer.position = data.position
 			socket.broadcast.emit('player_move', currentPlayer)
 		})
 
 		socket.on('enemy_move', (data) => {
-			console.log(`recv: move: ${JSON.stringify(data)}`)
-			// currentPlayer.position = data.position
-			// enemyA.position = data.position
+			// console.log(`recv: move: ${JSON.stringify(data)}`)
 			let e = enemies.find(enemy => enemy.name === data.name)
-			e.position = data.position
-			socket.broadcast.emit('enemy_move', e)
+			if (data.hasOwnProperty('position') && e != undefined) {
+				e.position = data.position
+				socket.broadcast.emit('enemy_move', e)
+			}
 		})
 
-		socket.on('player_shoot', (data) => {})
-		socket.on('player_bomb', (data) => {})
 		socket.on('player_action', (data) => {
-			console.log(`${currentPlayer.name} recv: action`)
+			// console.log(`${currentPlayer.name} recv: action`)
 			let value = {
-				name: currentPlayer.name,
-				type: data.type
+				...data,
+				name: currentPlayer.name
 			}
 			socket.emit('player_action', value)
 			socket.broadcast.emit('player_action', value)
 		})
-		socket.on('player_health', (data) => {})
-		socket.on('player_pick_item', (data) => {})
-		socket.on('wave', (data) => {})
+
+		socket.on('zombie_dead', (data) => {
+			enemies = enemies.map((enemy, index) => {
+				if (enemy.name === data.name) {
+					enemy.isDeath = true
+					score += 100
+					let playData = {
+						wave: currentWave,
+						score: score
+					}
+					socket.emit('play_data', playData)
+				}
+				return enemy
+			})
+		})
+
+		let genEnemy = () => {
+			let roundSpawn = 0
+			enemies = []
+			let data = {
+				wave: currentWave,
+				score: score
+			}
+			socket.emit('play_data', data)
+			let waveSpawnEnemy = setInterval(() => {
+				console.log(enemies.length)
+				roundSpawn++
+				let newEnemies = []
+				enemySpawnPoints.map(enemySpawnPoint => {
+					let newEnemy = {
+						name: Math.random().toString(36).substring(7),
+						position: enemySpawnPoint.position,
+						health: 10,
+						isDeath: false
+					}
+					enemies.push(newEnemy)
+					newEnemies.push(newEnemy)
+				})
+				let enemiesResponse = { enemies: newEnemies }
+				console.log(`${currentPlayer.name} emit: enemies: ${JSON.stringify(enemiesResponse)}`)
+				socket.emit('enemies', enemiesResponse)
+				socket.broadcast.emit('enemies', enemiesResponse)
+				if (roundSpawn >= wave[currentWave].n / enemySpawnPoints.length) {
+					++currentWave
+					isGenEnemy = false
+					clearInterval(waveSpawnEnemy)
+				}
+			}, wave[currentWave].delay)
+		}
+
+		socket.on('horizontal', (data) => {
+			// console.log(`${currentPlayer.name} recv: action`)
+			let value = {
+				...data,
+				name: currentPlayer.name
+			}
+			socket.emit('horizontal', value)
+			socket.broadcast.emit('horizontal', value)
+		})
 
 		socket.on('disconnect', () => {
 			console.log(`${currentPlayer.name} recv: disconnect ${currentPlayer.name}`)
@@ -106,9 +214,43 @@ let init = (server) => {
 			console.log(`${currentPlayer.name} bcst: other player disconnected ${currentPlayer}`)
 			let index = clients.findIndex(client => client.name === currentPlayer.name)
 			clients.splice(index, 1)
+			if (clients.length === 0) {
+				resetData()
+			}
 		})
-		// socket.on('player_pick_item', (data) => {})
-		// socket.on('item', (data) => {})
+
+		socket.on('dead', (data) => {
+			clients = clients.map(client => {
+				client.isDeath = true
+				return client
+			})
+			if (clients.filter(client => client.isDeath === false).length === 0) {
+				socket.emit('end', { score })
+				history.once('value')
+						.then((snapshots) => {
+							history.push({
+								id: snapshots.numChildren() + 1,
+								team: teamName,
+								nperson: clients.length,
+								score,
+								timestamp: firebase.database.ServerValue.TIMESTAMP
+							})
+							resetData()
+						})
+			}
+		})
+
+		socket.on('ranking', () => {
+			history.on('value', (snapshots) => {
+				const result = []
+				snapshots.forEach((snapshot) => {
+					result.push(snapshot.val())
+				})
+				let ranking = result.sort((a, b) => b.score - a.score).slice(0, 10)
+				console.log(ranking)
+				socket.emit('ranking', { ranking })
+			})
+		})
 	})
 }
 
